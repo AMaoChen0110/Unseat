84aaa052115d45d8e32abc25945d1e4d79326788
wen.you <wen.you27@icloud.com>
2025-04-08 23:37:24 +0800

    feat: 加上複製文字功能

--- scripts/complimentSquad.js
+++ scripts/complimentSquad.js
@@ -39,7 +39,7 @@ async function init() {
       minusBtn.textContent = '-';
       const input = document.createElement('input');
       input.type = 'number';
-      input.value = 1;
+      input.value = 0;
       input.min = 0;
       input.id = p.fullName;
       const plusBtn = document.createElement('button');
@@ -118,6 +118,26 @@ async function generateReport() {
 
   // 渲染前端
   toggleDialog();
+}
+
+function copyReport() {
+  const range = document.createRange();
+
+  // 將指定元素內容加到 Range 中
+  const texts = document.querySelector('.report-content');
+
+  range.selectNode(texts);
+  // 取得 Selection 物件
+  const selection = window.getSelection();
+  // 先清空當前選取範圍
+  selection.removeAllRanges();
+  // 加入 Range
+  selection.addRange(range);
+
+  document.execCommand('copy');
+  selection.removeAllRanges();
+
+  window.alert('複製成功');
 }
 
 init();
